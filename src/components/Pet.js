45911020eaa63fb37413fe481b5d66b6a09dc1b5
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2019-11-23 23:34:48 +0300

    version 1

---
+++ src/components/Pet.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function Pet(props) {
+    const {name, image, age, description, breed} = props;
+    return (
+        <div className="col-lg-6 col-md-4 mb-4">
+            <div className="card h-100" style={{marginTop: 15}}>
+                <a href="#"><img className="card-img-top" src={image} alt="" style={{height: "292px"}}/></a>
+                <div className="card-body">
+                    <h4 className="card-title">
+                        <a href="#">{name}</a>
+                        <div>
+                            <span className="badge badge-primary" style={{fontSize: "12px"}}>{breed}</span>
+                        </div>
+                        <div>
+                            <span className="badge badge-warning" style={{fontSize: "12px"}}>{age}</span>
+                        </div>
+                    </h4>
+                    <p className="card-text">
+                        {description}
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <div className="btn btn-success">Favorilere Ekle</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Pet;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Sidebar(props) {
+    return (
+        <div>
+            Sidebar
+        </div>
+    );
+}
+
+export default Sidebar;
